b76105befa8213920967bc9c0721edf6820af35d
vladyslavkozlov2101git <vladyslavkozlov2101git@gmail.com>
2020-05-23 11:02:28 +0300

    added followers feed

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,24 @@ $(function(){
         ratedFill: "#ffa726",
         readOnly: true
     });
+    
+    $('.f-slider__inner.feed').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+    });
+
     $('.f-slider__inner').slick({
         dots: false,
         arrows: true,
         slidesToShow: 1,
         slidesToScroll: 1,
     });
+    
+
+
+
     var mixer = mixitup('.release__items');  
 
     
